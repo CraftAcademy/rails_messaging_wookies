8be006f3eafa5b3a0f2b837ec8d1b71fda5d8f3a
Antonella Morittu <morittuantonella@gmail.com>
2018-02-23 09:46:49 +0100

    all green

--- features/step_definitions/user_logout_steps.rb
+++ features/step_definitions/user_logout_steps.rb
@@ -1,8 +1,3 @@
-Given("I am logged in as {string}") do |name|
-  user = User.find_by(name: name)
-  login_as(user, scope: :user)
-end
-
 When("I click on {string}") do |string|
   click_link(string)
 end
